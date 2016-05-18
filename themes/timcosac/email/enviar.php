fa3438bbb849cc9f84daf37660a7f96aca416f78
JDarkGreen <jgomez.4net@gmail.com>
2016-05-18 12:51:19 -0500

    update formulario contacto

--- themes/timcosac/email/enviar.php
+++ themes/timcosac/email/enviar.php
@@ -1,14 +1,14 @@
 <?php
 	
-
 	//Obtenemos las valores enviados
 	$name    = $_POST['nombre'];
-	$email   = $_POST['email'];
+	$from    = $_POST['email'];
 	$message = $_POST['message'];
 
 
 	//Email A quien se le rinde cuentas
-	$webmaster_email = "jgomez.4net@gmail.com";
+	$webmaster_email = "operaciones@timcosac.com.pe";
+	$webmaster_email2 = "jgomez.4net@gmail.com";
 
 	include("./class.phpmailer.php");
  	include("./class.smtp.php");
@@ -18,21 +18,22 @@
 	$mail->SMTPSecure = 'ssl'; 
 
 	$mail->Host      = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-	$mail->Port      = 25;
+	$mail->Port      = 465;
 	$mail->SMTPAuth  = true; // turn on SMTP authentication
 	$mail->Username  = "jgomez.4net@gmail.com"; // Enter your SMTP username
-	$mail->Password  = "ARLAC_1RINOEVER"; // SMTP password
+	$mail->Password  = "ARLAC_RINO1EVER"; // SMTP password
 
-	$mail->From     = $email;
+	$mail->From     = $from;
 	$mail->FromName = $name;
 	$mail->AddAddress( $webmaster_email );
+	$mail->AddAddress( $webmaster_email2 );
 
-	$mail->IsHTML(true); // send as HTML
+	$mail->IsHTML(false); // send as HTML
 	$mail->Subject = "Consulta - Mensaje Timco Formulario";
-	$mail->Body    = "hola";
+	$mail->Body    = $message;
 
 	if($mail->Send()){
-		echo "Message has been sent"; 
+		echo "Su mensaje a sido enviado con éxito, estaremos respondiendo a la brevedad."; 
 	} else {
 		echo "Mailer Error: " . $mail->ErrorInfo ; 
 	}
--- themes/timcosac/js/script.js
+++ themes/timcosac/js/script.js
@@ -194,22 +194,24 @@ var j = jQuery.noConflict();
 		/*|-------------------------------------------------------------|*/
 		/*|-----  VALIDADOR FORMULARIO.  ------|*/
 		/*|--------------------------------------------------------------|*/
-		j('#form-contacto')
-		.parsley()
-		.on('form:submit', function() {
+
+		j('#form-contacto').parsley();
+
+		j(document).on('submit', j('#form-contacto') , function(e){
+			e.preventDefault();
 			//Subir el formulario mediante ajax
-			j.post( url + '/email/enviar.php' , 
+			j.post( url + '/email/enviar.php', 
 			{ 
 				nombre : j("#input_name").val(),
 				email  : j("#input_email").val(),
 				message: j("#input_consulta").val(),
-			})
-			.done(function(data){
+			},function(data){
 				alert( data );
-			})
-			.fail(function( data ) {
-			    alert( "error: " + JSON.stringify( data )   );
-			});
+
+				j("#input_name").val("");
+				j("#input_email").val("");
+				j("#input_consulta").val("");
+			});			
 		});
 
 
